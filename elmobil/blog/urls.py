1c225557523106847e03958b7831ab2383e8427e
Badmajor <vtk88@mail.ru>
2023-09-07 14:21:40 +0300

    init commit

---
+++ elmobil/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post/<int:id>', views.post_detail, name='post'),
+    path('category/<slug:category_slug>/',
+         views.category_posts,
+         name='category_posts')
+
+]
