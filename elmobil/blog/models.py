e815062ddba1c06fce6537b82a8345da66bda1b0
Badmajor <vtk88@mail.ru>
2023-09-12 18:50:50 +0300

    add models Post, Autor, Categoru, Tags

--- elmobil/blog/models.py
+++ elmobil/blog/models.py
@@ -1,15 +1,38 @@
 from django.db import models
 
-from taggit.managers import TaggableManager
+
+class Category(models.Model):
+    name = models.CharField(max_length=56)
+
+    def __str__(self):
+        return self.name
 
 
-# Create your models here.
+class Autor(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=24)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=56)
+    post_url = models.URLField(default=title, max_length=75)
     date_of_create = models.DateField(auto_now_add=True)
     date_of_change = models.DateField(auto_now=True)
-    autor = models.CharField(max_length=56, default='ChatGPT')
+    autor = models.ForeignKey(Autor, default='autor', on_delete=models.PROTECT)
     text = models.TextField()
-    tags = TaggableManager()
+    views_count = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
+    category = models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tags)
 
-
+    def __str__(self):
+        return self.title
