5f3a7ee7f998c2794dac429a8fd0b2d38852b554
Badmajor <vtk88@mail.ru>
2023-09-13 14:13:48 +0300

    fixed index.html and interesting.html for models

--- elmobil/blog/urls.py
+++ elmobil/blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<int:id>', views.post_detail, name='post'),
+    path('post/<slug:post_slug>', views.post_detail, name='post'),
     path('category/<slug:category_slug>/',
          views.category_posts,
          name='category_posts')
--- elmobil/blog/views.py
+++ elmobil/blog/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 
-from .posts_list import posts
+from .models import Post, Category
 
 
 def index(request):
     template = 'blog/index.html'
+    posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, template, context)
 
 
-def post_detail(request, id: int):
+def post_detail(request, post_slug: str):
     template = 'blog/detail.html'
+    posts = Post.objects.all()
     for post in posts:
-        if post.get('id') == id:
+        if post.post_slug == post_slug:
             context = {'post': post}
             break
     else:
-        context = {'post': {'id': id,
+        context = {'post': {'id': 777,
                             'date': 'None',
                             'author': 'None',
                             'image_path': '1.png',
