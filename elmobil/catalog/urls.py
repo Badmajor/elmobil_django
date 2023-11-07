a0a17b19976e30c47a0c90434ce76d85b33815bb
Badmajor <vtk88@mail.ru>
2023-11-07 14:35:11 +0300

    add url catalog/pk

--- elmobil/catalog/urls.py
+++ elmobil/catalog/urls.py
@@ -7,6 +7,7 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.CarsListView.as_view(), name='car_list'),
     path('<int:pk>/<slug:slug>/', views.CarDetailView.as_view(), name='car_detail'),
+    path('<int:pk>/', views.CarDetailView.as_view(), name='car_detail_pk'),
     path(
         'manufacturer/<slug:slug>/',
         views.ManufacturerDetailView.as_view(),
