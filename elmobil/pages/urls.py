fa7f8ed0147fe5b05cf511244df9acb76878a16d
Badmajor <vtk88@mail.ru>
2024-02-12 15:38:46 +0300

    adding top_charge_speed

--- elmobil/pages/urls.py
+++ elmobil/pages/urls.py
@@ -10,7 +10,12 @@ urlpatterns = [
         'top-100-elektromobilej-po-zapasu-hoda',
         views.TopRangeListView.as_view(),
         name='top_range_100'
-    )
+    ),
+    path(
+        'top-100-elektromobilej-po-skorosti-zaryadki',
+        views.TopChargeTimeListView.as_view(),
+        name='top_time_charge_100'
+    ),
 
 ]
 
--- elmobil/pages/views.py
+++ elmobil/pages/views.py
@@ -14,28 +14,58 @@ def about(request):
     return render(request, template, context)
 
 
-class TopRangeListView(ListView):
+class BaseTopListView(ListView):
     model = Car
     paginate_by = 100
     paginate_orphans = 5
-    template_name = 'pages/top_range_list.html'
+    template_name = 'pages/top_list.html'
+    title_page = None
+    working_field = None
+    measure = None
 
 
     def get_queryset(self):
         queryset =  self.model.objects.select_related(
             'manufacturer',
-            'performance',
-        ).order_by('-performance__electric_range')
+            self.get_parent_field(),
+        ).order_by('-' + self.working_field)
         # Получаем максимальный запас хода
-        max_value = queryset.first().performance.electric_range
+        max_value = queryset.aggregate(max_value=models.Max(self.working_field))[
+            'max_value']
         # Добавляем процент от максимального значения в queryset
         queryset = queryset.annotate(
-            recent_to_max_range=models.ExpressionWrapper(
-                models.F('performance__electric_range') * 100 / max_value,
+            measure=models.Value(self.measure, output_field=models.TextField()),
+            value=models.F(self.working_field),
+            percent=models.ExpressionWrapper(
+                models.F('value') * 100 / max_value,
                 output_field=models.FloatField()
             )
         )
 
         return queryset
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title_page'] = self.title_page
+        return context
 
+    def get_parent_field(self):
+        if not self.working_field:
+            raise ValueError('Field: "working_field" is not filled')
+        parent_field = self.working_field.split('__')[0]
+        return parent_field
+
+
+
+class TopRangeListView(BaseTopListView):
+    working_field = 'performance__electric_range'
+    title_page = 'Топ 100 Электромобилей по запасу хода'
+    measure = 'км'
+
+
+
+
+class TopChargeTimeListView(BaseTopListView):
+    working_field = 'charging__charge_speed'
+    title_page = 'Топ 100 Электромобилей по скорости зарядки'
+    measure = 'км в час'
