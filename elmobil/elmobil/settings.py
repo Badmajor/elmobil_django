4a5d5c8aac685e1b2ec4c022171ea795a6d18fe9
Badmajor <v.balonkin@gmail.com>
2024-04-19 17:20:06 +0300

    add JWT token

--- elmobil/elmobil/settings.py
+++ elmobil/elmobil/settings.py
@@ -122,14 +122,14 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
 }
 
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+   'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DJOSER = {
