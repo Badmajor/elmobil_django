b6e136b4c520f5cbcace1fb32fa992df83d3a297
Badmajor <v.balonkin@gmail.com>
2024-04-26 12:32:08 +0300

    add API docimentation

--- elmobil/elmobil/urls.py
+++ elmobil/elmobil/urls.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.urls import path, include
+from django.urls import include, path, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 from blog.sitemap import PostSitemap
 from catalog.sitemap import CarsSitemap, ManufacturersSitemap
@@ -41,3 +44,24 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Elmobil API",
+      default_version='v1',
+      description="Документация для приложения elmobil",
+      contact=openapi.Contact(email="v.balonkin@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+       name='schema-swagger-ui'),
+   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+       name='schema-redoc'),
+]
